022db0dec82cd0d8bd491498c9ff857dfff3a29e
Brock Stoops <bstoops@digispade.com>
2015-03-07 16:54:39 -0500

    Create tower.java

---
+++ tower.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class tower {
+
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int numcases = in.nextInt();
+		
+		for(int i=0; i<numcases; i++){
+			System.out.println(in.nextInt());
+		}
+	}
+
+}
